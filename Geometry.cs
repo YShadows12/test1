b4fbb35bf3c5576467cab539af46c3819520fb98
YShadows12 <miguel19699@iesmarenostrum.com>
2020-09-29 20:00:06 +0200

    Geometry class
    He empezado la clase geometry

---
+++ Geometry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEST1
+{
+    class Geometry2d
+    {
+        public static double GetVectorLength(double x, double y)
+        {
+            double aux;
+            double length;
+            aux = x * x + y * y;
+            length = System.Math.Sqrt(aux);
+            return length;
+
+        }
+    }
+}
